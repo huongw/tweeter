2583de651c67d5b710b4be065b0a821ead74940f
huongw <whitneyhuong@gmail.com>
2021-09-07 22:29:05 +0000

    create char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#tweet-text").on("keyup", function() {
+    const textLength = this.textLength;
+    const $counter = $(this).parent().siblings(".button-wrapper").children(".counter");
+    const counterMaxLength = 140;
+    $counter.val(counterMaxLength - textLength);
+    if ($counter.val() < 0){
+      $counter.css("color", "red");
+    }
+  })
+});
