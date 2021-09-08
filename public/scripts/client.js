07490b8fa9e560df25adb6c3c699b553b094e22d
huongw <whitneyhuong@gmail.com>
2021-09-08 22:32:15 +0000

    use ajax for get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,55 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const createTweetElement = function(tweet) {
+  let $tweet = `
+  <article class="tweet">
+  <header>
+    <div class="name-wrapper">
+      <img src="${tweet.user.avatars}">
+      <span>${tweet.user.name}</span>
+    </div>
+    <div>
+      <span><a href="#">${tweet.user.handle}</a></span>
+    </div>
+  </header>
+  <div class="tweet-post">
+    <p>${tweet.content.text}</p>
+  </div>
+  <footer>
+    <span>
+      ${timeago.format(tweet["created_at"])}
+    </span>
+    <span class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </span>
+  </footer>
+</article>
+`
+return $tweet;
+};
 
+$(document).ready(function() {
+
+  const renderTweets = function(tweets) {
+    for (const obj of tweets) {
+      $('#tweets-container').append(createTweetElement(obj));
+    }
+  };
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function(arr) {
+      console.log("Success", arr);
+      renderTweets(arr);
+    })
+  };
+
+  loadTweets()
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    const data = $(this).serialize()
+    $.post('/tweets', data)
+  })
+
+})
